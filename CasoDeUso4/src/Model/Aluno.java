774b0c5d38f195078172c0618ac22479ca3cbdf5
Arthurchinamatsda <ademarteiyuchina@gmail.com>
2025-06-23 23:10:47 -0300

    4° caso de uso

---
+++ CasoDeUso4/src/Model/Aluno.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class Aluno {
+    private String nome;
+    private int idade;
+    private String matricula;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
